a88f6ed06fae738c808f9ce6f12507e81cdca4ad
shubhrai23 <shubhrai0205@gmail.com>
2026-09-08 12:58:54 +0530

    fix: use SVG icon instead of x character for clear tag button

--- static/skin/index.js
+++ static/skin/index.js
@@ -481,7 +481,7 @@
     function addTagElement(tagValue, resetFilter) {
         const tagElement = document.getElementsByClassName('tagFilterLabel')[0];
         tagElement.style.display = 'inline-block';
-        tagElement.innerHTML = htmlEncode(tagValue) + ' &times;';
+        tagElement.innerHTML = htmlEncode(tagValue) + ' <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin-left: 4px; margin-bottom: 2px;"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
         const tagMessage = $t("stop-filtering-by-tag", {TAG: tagValue});
         tagElement.setAttribute('aria-label', tagMessage);
         tagElement.setAttribute('title', tagMessage);
